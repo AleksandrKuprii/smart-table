bf2ec7c147e25301f9503dd398da50d01d191d22
Aleksandr Kupriianov <kupr9889kupr@gmail.com>
2026-09-03 13:58:38 +0300

    Проектная работа 6.2

--- src/components/searching.js
+++ src/components/searching.js
@@ -3,9 +3,9 @@ import {rules, createComparison} from "../lib/compare.js";
 
 export function initSearching(searchField) {
     // @todo: #5.1 — настроить компаратор
-
+    
     return (data, state, action) => {
         // @todo: #5.2 — применить компаратор
-        return data;
+        
     }
 }
